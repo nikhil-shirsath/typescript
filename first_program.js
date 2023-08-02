403b6b96b60632151302f877bab37374ee2886e0
nikhil shirsath <001nikhilshirsath001@gmail.com>
2023-08-02 17:31:03 +0530

    day 1

---
+++ first_program.js
@@ -0,0 +1,4 @@
+var msg = "hello Typescript programming ,I am Nikhil Shirsath";
+console.log(msg);
+document.write(msg);
+alert("hello nikhil ");
---
+++ first_program.ts
@@ -0,0 +1,6 @@
+var msg ="hello Typescript programming ,I am Nikhil Shirsath"
+
+console.log(msg)
+document.write(msg)
+
+alert("hello nikhil ")
