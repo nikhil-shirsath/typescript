81ee74dae897eafe701e8503af3e53203710b3c7
nikhil shirsath <001nikhilshirsath001@gmail.com>
2023-08-16 18:18:13 +0530

    decision making theory

---
+++ decision_making.ts
@@ -0,0 +1,14 @@
+// decision making
+
+// *decision making structure require that the programmer specifies one or more conditions
+// to be evaluated or tested by the program, along with the statement or statementsto be executed
+// if the condition is determined to be true, and optionally to be true , other statements to be 
+// executed if the condition is determined to be false.
+
+// decision-making construct evaluates a condition before the instrunctions ate executed.
+// Decision making constrncts in typescript are classified as follows.
+
+// 1. if condition 
+// 2. if..else condition
+// 3. else if and nested if conditional statenents
+// 4. switch statement 
